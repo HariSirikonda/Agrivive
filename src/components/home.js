a5982afdc131bd0cab79d1aa20614d0dad0d6104
Hari Sirikonda <harisirikonda22@gmail.com>
2024-11-26 02:20:48 +0530

    Home updated

--- src/components/home.js
+++ src/components/home.js
@@ -12,10 +12,11 @@ function Home() {
             <section className='Intro container-fluid'>
                 <div className='row m-2 p-2'>
                     <div className='col-8 d-flex align-items-center justify-content-center  '>
-                        <div className='squareContainer'>
-                            <h4>Agrivive Welcomes you</h4>
+                        <div className='squareContainer p-5'>
+                            <h4>Agrivive Welcomes you...</h4>
                             <h1 className='text-success'>Empowering farmers and Nuturing India's Future</h1>
-                            <p className='lead'><b>100%</b> satisfied <b>Yeild Returns</b></p>
+                            <p className='lead'>Looking for an oppurtunity to make use of your unused lands ? You have come to the right place... We are here to help you earn a considerable income from your land. handover your land for a considerable income monthly on time. Who knows ? your financial freedon might start over here with us. Your liabilities turns Assets. What are you waiting for ? Sign up fast and start your journey towards financial freedom.</p>
+                            <p className='lead text-success'><b>100%</b> satisfied <b>Yeild Returns</b></p>
                             <button className='btn btn-success m-2' type='submit'>Register</button>
                             <button className='btn btn-light m-2' type='submit'>Login</button>
                         </div>
