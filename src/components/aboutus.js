009e8bd4d00bac0165bd42097118e20a40d607a7
Hari Sirikonda <harisirikonda22@gmail.com>
2024-11-25 22:19:13 +0530

    Changed font

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -9,57 +9,57 @@ function AboutUs() {
                     <div class="row mt-5">
                         <div class="col-md-8">
                             <div className="discription text-start mx-2 my-3 p-2">
-                                <h2 className="text-success p-2">About Agrivive ?</h2>
-                                <p className="mx-2">Agrivive is set to make use of unused farming lands in India. Our main motive is to empower the Agriculture production in our country. We a vide collection of eperts for land nutrition testing, Crop Analysis, Yield Estimation, Marketing Department and also ligal Department. </p>
+                                <h1 className="text-success p-2">About Agrivive ?</h1>
+                                <p className="mx-2 lead">Agrivive is set to make use of unused farming lands in India. Our main motive is to empower the Agriculture production in our country. We a vide collection of eperts for land nutrition testing, Crop Analysis, Yield Estimation, Marketing Department and also ligal Department. </p>
                                 <ul>
                                     <li>
-                                        <p><b>Land nutrition Testing : </b>We perform nutrition test for the soil at your piece of Land. The soil Nutrition Experts come to place and performs the analysis infront of your presence and the reveals the reports then itself. The whole process goes very trasparent andd trusted way by the land owners. We have the best Soil nutrition analysts in our hands for deciding what crop to be started inorder to get the most satisfying results.</p>
+                                        <p className="lead"><b>Land nutrition Testing : </b>We perform nutrition test for the soil at your piece of Land. The soil Nutrition Experts come to place and performs the analysis infront of your presence and the reveals the reports then itself. The whole process goes very trasparent andd trusted way by the land owners. We have the best Soil nutrition analysts in our hands for deciding what crop to be started inorder to get the most satisfying results.</p>
                                     </li>
                                     <li>
-                                        <p><b>Crop Analyts : </b> After completing the nutrition test on the land, the crop analysts take the charge. They analyse the financial adn economical aspects of the land which will bring us the best results financially as well as Economically.</p>
+                                        <p className="lead"><b>Crop Analyts : </b> After completing the nutrition test on the land, the crop analysts take the charge. They analyse the financial adn economical aspects of the land which will bring us the best results financially as well as Economically.</p>
                                     </li>
                                     <li>
-                                        <p><b>Marketing Agents : </b> We never let our customers to bother about the marketing work of the crop. we have the best marketing agents who deals with your crop. Our customer need not to worry about the present or future circumstances, obstacles for the marketing of the crop. Our marketing agents are taken into the company such that they deliver a satisfying feel to the customers at any cost.</p>
+                                        <p className="lead"><b>Marketing Agents : </b> We never let our customers to bother about the marketing work of the crop. we have the best marketing agents who deals with your crop. Our customer need not to worry about the present or future circumstances, obstacles for the marketing of the crop. Our marketing agents are taken into the company such that they deliver a satisfying feel to the customers at any cost.</p>
                                     </li>
                                     <li>
-                                        <p><b>Ligal Activity : </b> Your land will be under the Ligal department of our company untill term ends. There will be bononus and insentives for the customers who gives the nutrition rich land for the rented farm procedure. Our ligal team will be with you even after ending the term document. A customer is always a customer for us. You can come and meet our Ligal team for any suggestions related to the land purchase or selling.</p>
+                                        <p className="lead"><b>Ligal Activity : </b> Your land will be under the Ligal department of our company untill term ends. There will be bononus and insentives for the customers who gives the nutrition rich land for the rented farm procedure. Our ligal team will be with you even after ending the term document. A customer is always a customer for us. You can come and meet our Ligal team for any suggestions related to the land purchase or selling.</p>
                                     </li>
                                 </ul>
                             </div>
                             <hr />
                             <div className="discription text-start mx-5 my-3 p-2">
-                                <h2 className="text-success">Our Comprehensive Support Services</h2>
-                                <p className="mx-2">To ensure the success of farmers, we offer a wide range of services:</p>
+                                <h1 className="text-success p-2">Our Comprehensive Support Services</h1>
+                                <p className="mx-2 lead">To ensure the success of farmers, we offer a wide range of services:</p>
                                 <ul>
                                     <li>
-                                        <p><b>Agricultural Advisory Services:</b>Our team of agricultural experts provides tailored advice on crop selection, pest and disease management, soil health, and sustainable farming practices.</p>
+                                        <p className="lead"><b>Agricultural Advisory Services:</b>Our team of agricultural experts provides tailored advice on crop selection, pest and disease management, soil health, and sustainable farming practices.</p>
                                     </li>
                                     <li>
-                                        <p><b>Technology Solutions: </b>We leverage cutting-edge technology to enhance farm productivity and efficiency. Our digital platforms offer real-time weather updates, market information, and expert guidance.</p>
+                                        <p className="lead"><b>Technology Solutions: </b>We leverage cutting-edge technology to enhance farm productivity and efficiency. Our digital platforms offer real-time weather updates, market information, and expert guidance.</p>
                                     </li>
                                     <li>
-                                        <p><b>Input Supply:</b>We provide high-quality seeds, fertilizers, and pesticides to farmers, ensuring optimal crop yields and quality.</p>
+                                        <p className="lead"><b>Input Supply:</b>We provide high-quality seeds, fertilizers, and pesticides to farmers, ensuring optimal crop yields and quality.</p>
                                     </li>
                                     <li>
-                                        <p><b>Market Access:</b>We connect farmers to domestic and international markets, facilitating the sale of their produce at competitive prices.</p>
+                                        <p className="lead"><b>Market Access:</b>We connect farmers to domestic and international markets, facilitating the sale of their produce at competitive prices.</p>
                                     </li>
                                     <li>
-                                        <p><b>Capacity Building:</b>We organize training programs and workshops to equip farmers with the skills and knowledge they need to thrive in a changing agricultural landscape.</p>
+                                        <p className="lead"><b>Capacity Building:</b>We organize training programs and workshops to equip farmers with the skills and knowledge they need to thrive in a changing agricultural landscape.</p>
                                     </li>
                                 </ul>
                             </div>
                         </div>
                         <div class="col-md-4 p-3 bg-success text-light align-items-centers ">
-                            <h2>Our Vision</h2>
-                            <p>To be the leading force in Indian agriculture, revolutionizing farming practices and ensuring food security for generations to come. We envision a future where farmers are empowered, agriculture is sustainable, and rural India flourishes.</p>
-                            <h2>Our Mission</h2>
-                            <p>To empower Indian farmers by providing innovative solutions and unwavering support. We strive to enhance agricultural productivity, sustainability, and profitability, transforming rural livelihoods. Through cutting-edge technology, financial assistance, and expert guidance, we aim to create a thriving agricultural ecosystem that benefits both farmers and consumers.<br />By fostering sustainable practices and empowering rural communities, we contribute to a greener and more prosperous India.</p>
+                            <h1>Our Vision</h1>
+                            <p className="lead">To be the leading force in Indian agriculture, revolutionizing farming practices and ensuring food security for generations to come. We envision a future where farmers are empowered, agriculture is sustainable, and rural India flourishes.</p>
+                            <h1>Our Mission</h1>
+                            <p className="lead">To empower Indian farmers by providing innovative solutions and unwavering support. We strive to enhance agricultural productivity, sustainability, and profitability, transforming rural livelihoods. Through cutting-edge technology, financial assistance, and expert guidance, we aim to create a thriving agricultural ecosystem that benefits both farmers and consumers.<br />By fostering sustainable practices and empowering rural communities, we contribute to a greener and more prosperous India.</p>
                             <div className="text-start">
-                                <h2 className="m-3 text-center">Contact Us </h2>
+                                <h1 className="m-3 text-center">Contact Us </h1>
                                 <hr />
-                                <p><b>Phone numebr : </b>+91 99******123</p>
-                                <p><b>Email :</b> farmsupport@gmail.com</p>
-                                <p><b>Address : </b> H.no - 1-2-235,Hanuman line, Maisammaguda, medchal dist, Hyderabad, Talangana, India</p>
+                                <p className="lead"><b>Phone numebr : </b>+91 99******123</p>
+                                <p className="lead"><b>Email :</b> farmsupport@gmail.com</p>
+                                <p className="lead"><b>Address : </b> H.no - 1-2-235,Hanuman line, Maisammaguda, medchal dist, Hyderabad, Talangana, India</p>
                             </div>
                             <div>
                                 <a className="m-2 text-light" href="https://facebook.com" target="_blank"><i class="bi bi-facebook"></i></a>
