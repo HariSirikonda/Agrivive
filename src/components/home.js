550980e55c1b11412d6676242feb7b10046adcaa
Hari Sirikonda <harisirikonda22@gmail.com>
2024-11-25 22:05:19 +0530

    Changed to section

--- src/components/home.js
+++ src/components/home.js
@@ -9,13 +9,13 @@ function Home() {
     };
     return (
         <>
-            <div className='Intro container-fluid'>
+            <section className='Intro container-fluid'>
                 <div className='row m-2 p-2'>
                     <div className='col-8 d-flex align-items-center justify-content-center  '>
                         <div className='squareContainer'>
                             <h4>Agrivive Welcomes you</h4>
                             <h1 className='text-success'>Empowering farmers and Nuturing India's Future</h1>
-                            <p><b>100%</b> satisfied <b>Yeild Returns</b></p>
+                            <p className='lead'><b>100%</b> satisfied <b>Yeild Returns</b></p>
                             <button className='btn btn-success m-2' type='submit'>Register</button>
                             <button className='btn btn-light m-2' type='submit'>Login</button>
                         </div>
@@ -24,7 +24,7 @@ function Home() {
                         <img src={Img3} alt='show me'></img>
                     </div>
                 </div>
-            </div>
+            </section>
         </>
     );
 }
