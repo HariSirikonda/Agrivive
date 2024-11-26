11464aad97cbab516e9fc9ca800af5af24384125
Hari Sirikonda <harisirikonda22@gmail.com>
2024-11-26 10:25:06 +0530

    Added a news letter area

--- src/components/home.js
+++ src/components/home.js
@@ -15,10 +15,15 @@ function Home() {
                         <div className='squareContainer p-5'>
                             <h4>Agrivive Welcomes you...</h4>
                             <h1 className='text-success'>Empowering farmers and Nuturing India's Future</h1>
-                            <p className='lead'>Looking for an oppurtunity to make use of your unused lands ? You have come to the right place... We are here to help you earn a considerable income from your land. handover your land for a considerable income monthly on time. Who knows ? your financial freedon might start over here with us. Your liabilities turns Assets. What are you waiting for ? Sign up fast and start your journey towards financial freedom.</p>
+                            <p>Looking for an oppurtunity to make use of your unused lands ? You have come to the right place... We are here to help you earn a considerable income from your land. handover your land for a considerable income monthly on time. Who knows ? your financial freedon might start over here with us. Your liabilities turns Assets. What are you waiting for ? Sign up fast and start your journey towards financial freedom.</p>
                             <p className='lead text-success'><b>100%</b> satisfied <b>Yeild Returns</b></p>
                             <button className='btn btn-success m-2' type='submit'>Register</button>
                             <button className='btn btn-light m-2' type='submit'>Login</button>
+                            <div className='newsletter bg-light d-flex m-3 p-3 align-items-center justify-content-center'>
+                                <h4 className='w-50'>Subscribe for our news letter</h4>
+                                <input className='form-control m-2 w-50' placeholder='Enter your Email Id' id='Emailinput'></input>
+                                <button className='btn btn-success m-2 w-25' type='submit' >Submit</button>
+                            </div>
                         </div>
                     </div>
                     <div className='col-4'>
