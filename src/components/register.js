b2d9d52f6f7e8f41d4134d0e07e438928df03d7d
Hari Sirikonda <harisirikonda22@gmail.com>
2024-11-27 15:43:26 +0530

    Chnged register page for a suitable inputs

--- src/components/register.js
+++ src/components/register.js
@@ -14,10 +14,7 @@ function Register(){
                             <div className="logo">
                                 <img className="img-fluid p-2 w-25" src={Logo} alt="Logo" />
                             </div>
-                            <h3 className="text-dark fw-bolder fs-4 mb-2">Sign into Agrivive</h3>
-                            <div className="fw-normal text-muted mb-2">
-                                New Here? <a href="#" className="text-success text-decoration-none fw-bold">Create an Account</a>
-                            </div>
+                            <h3 className="text-dark fw-bolder fs-4 mb-2">Sign Up to Agrivive</h3>
                             <div className='d-flex align-items-center justify-content-center'>
                                 <div className="form-check mx-2 m-3">
                                     <input className="form-check-input" type="radio" name="exampleRadio" id="LandOwner" value="option1" />
@@ -34,31 +31,25 @@ function Register(){
                             </div>
                             <div className="form-floating mb-2 ">
                                 <input type="email" className="form-control" id="Username" placeholder="name@example.com" />
+                                <label for="floatingInput">First Name</label>
+                            </div>
+                            <div className="form-floating mb-2 ">
+                                <input type="email" className="form-control" id="Username" placeholder="name@example.com" />
+                                <label for="floatingInput">Last Name</label>
+                            </div>
+                            <div className="form-floating mb-2 ">
+                                <input type="email" className="form-control" id="Username" placeholder="name@example.com" />
                                 <label for="floatingInput">Email address</label>
                             </div>
-                            <div className="form-floating ">
+                            <div className="form-floating mb-2">
                                 <input type="password" className="form-control" id="Password" placeholder="Password" />
                                 <label for="floatingPassword">Password</label>
                             </div>
-                            <div className="mt-2 text-end">
-                                <Link className="text-secondary text-decoration-none fw-bold" to="/register/forgotPassword">Forget Password</Link>
+                            <div className="form-floating ">
+                                <input type="password" className="form-control" id="Password" placeholder="Password" />
+                                <label for="floatingPassword">Confirm Password</label>
                             </div>
-                            <button id="ContinueButton" type="submit" className="submit_btn btn btn-md btn-success w-100 my-4 p-2">Continue</button>
-                            <div className="text-center text-uppercase text-muted mb-3"><b>OR</b></div>
-                            <div className='m-2 d-block'>
-                                <div className='d-block bg-light m-2'>
-                                    <img className='me-2 p-1' src={GoogleIcon}></img>
-                                    <Link to={"https://www.google.com"} target='_blank' className='p-2 text-dark text-decoration-none me-2 p-2'>Continue with google</Link>
-                                </div>
-                                <div className='d-block bg-light m-2'>
-                                    <img className='me-2 p-1' src={FacebookIcon}></img>
-                                    <Link to={"https://www.facebook.com"} target='_blank' className='p-2 text-dark text-decoration-none me-2 p-2'>Continue with facebook</Link>
-                                </div>
-                                <div className='d-block bg-light m-2'>
-                                    <img className='me-2 p-1' src={LinkedIcon}></img>
-                                    <Link to={"https://www.linkedin.com"} target='_blank' className='p-2 text-dark text-decoration-none me-2 p-2'>Continue with linkedin</Link>
-                                </div>
-                            </div>
+                            <button id="ContinueButton" type="submit" className="submit_btn btn btn-md btn-success w-100 my-4 p-2">Create an Account</button>
                         </form>
                     </div>
                 </div>
