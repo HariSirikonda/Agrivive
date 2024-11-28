d8285971df75c7c7ad93673e23f800ac54e6d0ba
Hari Sirikonda <harisirikonda22@gmail.com>
2024-11-28 15:30:34 +0530

    Added border to the login page

--- src/components/home.js
+++ src/components/home.js
@@ -50,7 +50,7 @@ function Home() {
                             <p>Looking for an oppurtunity to make use of your unused lands ? You have come to the right place... We are here to help you earn a considerable income from your land. handover your land for a considerable income monthly on time. Who knows ? your financial freedon might start over here with us. Your liabilities turns Assets. What are you waiting for ? Sign up fast and start your journey towards financial freedom.</p>
                             <p className='lead text-success'><b>100%</b> satisfied <b>Yeild Returns</b></p>
                             <button className='btn btn-success m-2' type='submit' onClick={handleRegisterClick}>Register</button>
-                            <button className='btn btn-light m-2' type='submit' onClick={handleLoginClick}>Login</button>
+                            <button className='btn btn-light m-2 border' type='submit' onClick={handleLoginClick}>Login</button>
                             <div className='newsletter bg-light d-flex m-3 p-3 align-items-center justify-content-center'>
                                 <h4 className='w-50'>Subscribe for our news letter</h4>
                                 <input className='form-control m-2 w-50' placeholder='Enter your Email Id' id='Emailinput' type='email'></input>
