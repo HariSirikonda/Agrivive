acf263e1dea66dffb2697203084abe40cf40aac5
Hari Sirikonda <harisirikonda22@gmail.com>
2024-12-01 14:22:26 +0530

    Added the Services heading to the services page

--- src/components/services.js
+++ src/components/services.js
@@ -1,9 +1,13 @@
 function Services() {
     return (
         <>
-            <div className="container justify-content-center">
-                <h2>This is the Services page</h2>
-            </div>
+            <section className="container-fluid border p-3 d-flex align-items-center justify-content-center">
+                <div>
+                    <h1 className="display-4 text-center text-success">Our Services</h1>
+                    <p>Agrivive is full of services that a former as well as an Agricultural land owner needs. You just need to join us for all the services we offer. All in one service within one place. Agrivive the life of Land</p>
+                    <hr/>
+                </div>
+            </section>
         </>
     );
 }
