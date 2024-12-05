0e3a22383da19e1f0eecd7559b883a60892dda06
Hari Sirikonda <harisirikonda22@gmail.com>
2024-12-05 20:37:42 +0530

    Added The items into rows and columns

--- src/components/shop.js
+++ src/components/shop.js
@@ -9,11 +9,19 @@ function Shop() {
                     <p>We have brought the most good looking home decorative plants here at Agrivive's Shoping</p>
                     <hr />
                 </div>
-                <div className="container border d-flex align-items-center justify-content-center p-3">
-                    <Item name="Kwansen Tree" cost="124.95" Off="17%" />
-                    <Item name="Dalicus Tree" cost="150.00" Off="10%" />
-                    <Item name="Dalicus Tree" cost="150.00" Off="10%" />
-                    <Item name="Dalicus Tree" cost="150.00" Off="10%" />
+                <div className="container border">
+                    <div className="d-flex align-items-center justify-content-center">
+                        <Item name="Kwansen Tree" cost="124.95" Off="17%" />
+                        <Item name="Dalicus Tree" cost="150.00" Off="10%" />
+                        <Item name="Dalicus Tree" cost="150.00" Off="10%" />
+                        <Item name="Dalicus Tree" cost="150.00" Off="10%" />
+                    </div>
+                    <div className="d-flex align-items-center justify-content-center">
+                        <Item name="Kwansen Tree" cost="124.95" Off="17%" />
+                        <Item name="Dalicus Tree" cost="150.00" Off="10%" />
+                        <Item name="Dalicus Tree" cost="150.00" Off="10%" />
+                        <Item name="Dalicus Tree" cost="150.00" Off="10%" />
+                    </div>
                 </div>
             </section>
         </>
